aafe3e08722d4b153b74573c39e5f590c13cdae1
Liwei0503 <liwei0503@gmail.com>
2024-09-29 13:14:27 +0800

    Update setup_Pi5.sh

--- setup_Pi5.sh
+++ setup_Pi5.sh
@@ -1,2 +1,16 @@
 # (1) 安裝 python3-venv，以便創建虛擬環境：
 sudo apt install python3-venv
+# (2) 創建虛擬環境：
+# 在專案目錄中創建一個虛擬環境來隔離套件，假設目錄名稱為 myenv：
+python3 -m venv myenv
+# (3) 啟動虛擬環境：
+#     啟動後，會發現命令提示符前面會多一個 (myenv)，表示已進入虛擬環境。
+source myenv/bin/activate
+# (4) 使用 pip 安裝 pyautogui：
+#     這樣安裝的 pyautogui 只會在這個虛擬環境內可用，不會影響系統全域套件。
+pip install pyautogui
+# (5) 測試安裝是否成功：
+python -c "import pyautogui; print(pyautogui)"
+
+
+
